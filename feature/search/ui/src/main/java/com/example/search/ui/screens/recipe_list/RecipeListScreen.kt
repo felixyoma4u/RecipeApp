382c6ee89e79f6a67fd4631f5a93a4420f3f85f4
felixonoberevune <felix.onoberevune@iisysgroup.com>
2024-08-09 17:38:59 +0100

    refactor recipe list screen

--- feature/search/ui/src/main/java/com/example/search/ui/screens/recipe_list/RecipeListScreen.kt
+++ feature/search/ui/src/main/java/com/example/search/ui/screens/recipe_list/RecipeListScreen.kt
@@ -44,6 +44,7 @@ import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.common.navigation.NavigationRoutes
 import com.example.common.utils.UiText
+import com.example.search.domain.model.Recipe
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 
@@ -134,81 +135,94 @@ fun RecipeListScreen(
                     .padding(it)
                     .fillMaxSize()
             ) {
-                items(list) {
-                    Card(
-                        modifier = Modifier
-                            .padding(horizontal = 12.dp, vertical = 4.dp)
-                            .clickable {
-                                onEvent(RecipeList.Event.GotoRecipeDetails(it.idMeal))
-                            },
-                        shape = RoundedCornerShape(12.dp)
-                    ) {
-                        AsyncImage(
-                            model = it.strMealThumb,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(250.dp),
-                            contentScale = ContentScale.Crop
-                        )
-
-                        Spacer(modifier = Modifier.height(12.dp))
-
-                        Column(
-                            modifier = Modifier
-                                .padding(vertical = 12.dp)
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp)
-                        ) {
-
-                            Text(text = it.strMeal, style = MaterialTheme.typography.bodyLarge)
-                            Spacer(modifier = Modifier.height(12.dp))
-                            Text(
-                                text = it.strInstructions,
-                                style = MaterialTheme.typography.bodyMedium,
-                                overflow = TextOverflow.Ellipsis,
-                                maxLines = 4
-                            )
-                            Spacer(modifier = Modifier.height(12.dp))
-                            if (it.strTags.isNotEmpty()) {
-                                FlowRow {
-                                    it.strTags.split(",")
-                                        .forEach {
-                                            Box(
-                                                modifier = Modifier
-                                                    .padding(horizontal = 8.dp, vertical = 4.dp)
-                                                    .wrapContentSize()
-                                                    .background(
-                                                        color = Color.White,
-                                                        shape = RoundedCornerShape(24.dp)
-                                                    )
-                                                    .clip(shape = RoundedCornerShape(24.dp))
-                                                    .border(
-                                                        width = 1.dp,
-                                                        color = Color.Black,
-                                                        shape = RoundedCornerShape(24.dp)
-                                                    ),
-                                                contentAlignment = Alignment.Center
-                                            ) {
-                                                Text(
-                                                    text = it,
-                                                    style = MaterialTheme.typography.bodySmall,
-                                                    modifier = Modifier.padding(
-                                                        horizontal = 12.dp,
-                                                        vertical = 6.dp
-                                                    )
-                                                )
-                                            }
-                                        }
-                                }
-
-                                Spacer(modifier = Modifier.height(12.dp))
-                            }
-                        }
+                items(list) { recipe ->
+                    RecipeListItem(recipe = recipe) {
+                        onEvent(RecipeList.Event.GotoRecipeDetails(recipe.idMeal))
                     }
                 }
             }
         }
 
     }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun RecipeListItem(
+    modifier: Modifier = Modifier,
+    recipe: Recipe,
+    onClick: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .padding(horizontal = 12.dp, vertical = 4.dp)
+            .clickable {
+                onClick.invoke()
+            },
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        AsyncImage(
+            model = recipe.strMealThumb,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Column(
+            modifier = Modifier
+                .padding(vertical = 12.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+
+            Text(text = recipe.strMeal, style = MaterialTheme.typography.bodyLarge)
+            Spacer(modifier = Modifier.height(12.dp))
+            Text(
+                text = recipe.strInstructions,
+                style = MaterialTheme.typography.bodyMedium,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 4
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            if (recipe.strTags.isNotEmpty()) {
+                FlowRow {
+                    recipe.strTags.split(",")
+                        .forEach {
+                            Box(
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp, vertical = 4.dp)
+                                    .wrapContentSize()
+                                    .background(
+                                        color = Color.White,
+                                        shape = RoundedCornerShape(24.dp)
+                                    )
+                                    .clip(shape = RoundedCornerShape(24.dp))
+                                    .border(
+                                        width = 1.dp,
+                                        color = Color.Black,
+                                        shape = RoundedCornerShape(24.dp)
+                                    ),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Text(
+                                    text = it,
+                                    style = MaterialTheme.typography.bodySmall,
+                                    modifier = Modifier.padding(
+                                        horizontal = 12.dp,
+                                        vertical = 6.dp
+                                    )
+                                )
+                            }
+                        }
+                }
+
+                Spacer(modifier = Modifier.height(12.dp))
+            }
+        }
+    }
+
 }
